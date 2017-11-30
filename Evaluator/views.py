34f51f867bca9aac7934adeefe9242106a490ab6
arindam31 <arindam31@yahoo.co.in>
2017-12-01 02:56:54 +0530

    First commit

---
+++ Evaluator/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('This is the index page')
+
+def login(request):
+    return render(request, 'login.html')
