ccd88b1e47ed95802ed1bfb762ce67b8df2b4716
arindam31 <arindam31@yahoo.co.in>
2017-12-06 02:03:35 +0530

    MOdel changed so multiple interview can be created for the same candi

--- Evaluator/views.py
+++ Evaluator/views.py
@@ -4,11 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from .models import Interview
 
 # Create your views here.
 def index(request):
     return render(request, 'home.html')
 
 def profile(request):
-    args = {'user':request.user}
+    i = Interview()
+    print i.interviews_today()
+    args = {'user':request.user, 'interview_today': i.interviews_today()}
     return render(request, 'profile.html')
