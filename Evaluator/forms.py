1c756876769b04706ba98c8fd19021c85d947a02
arindam31 <arindam31@yahoo.co.in>
2017-12-09 19:06:07 +0530

    Registation page working fine

---
+++ Evaluator/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.admin.models import models
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2' )
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False) # Don't save it yet
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+
+        return user
+
--- Evaluator/views.py
+++ Evaluator/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import Interview
-from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
 
 # Create your views here.
 def index(request):
@@ -20,12 +20,12 @@ def profile(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('/')
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
         args = {'form': form}
         return render(request, 'register.html', args)
 
