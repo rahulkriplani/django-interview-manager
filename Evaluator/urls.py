69b6dc41678ed21505e1855774ce2f4435ebec80
arindam31 <arindam31@yahoo.co.in>
2017-12-11 00:24:39 +0530

    DB change. Fields added in interview model. COnditions navbar options for logged in user

--- Evaluator/urls.py
+++ Evaluator/urls.py
@@ -5,9 +5,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', login, {'template_name': 'login.html'}),
-    url(r'^logout/$', login, {'template_name': 'logout.html'}),
+    url(r'^logout/$', logout, {'template_name': 'logout.html'}),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'^register/$', views.register, name='register'),
     url(r'^profile/edit$', views.edit_profile, name='edit_profile'),
-    url(r'^profile/password/$', views.change_password, name='change_password'),
+    url(r'^password/$', views.change_password, name='change_password'),
     ]
