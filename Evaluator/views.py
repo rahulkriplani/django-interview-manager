d7491cd598e3cac5c293ac8f6081ccd3e493dd9a
arindam31 <arindam31@yahoo.co.in>
2017-12-11 00:52:51 +0530

    Certain pages now need login_required decorator

--- Evaluator/views.py
+++ Evaluator/views.py
@@ -2,16 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
-
 from .models import Interview
 from .forms import RegistrationForm, EditProfileForm
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, 'home.html')
 
+@login_required
 def profile(request):
     i = Interview()
     print i.interviews_today()
@@ -30,6 +31,7 @@ def register(request):
         args = {'form': form}
         return render(request, 'register.html', args)
 
+@login_required
 def edit_profile(request):
     if request.method == 'POST':
         form = EditProfileForm(request.POST, instance=request.user)
@@ -41,6 +43,7 @@ def edit_profile(request):
         args = {'form':form}
         return render(request, 'edit_profile.html', args)
 
+@login_required
 def change_password(request):
     if request.method == 'POST':
         form = PasswordChangeForm(data=request.POST, user=request.user)
