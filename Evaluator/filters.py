58b995176f0a4f6fc024bf0cd445eea7c1c345ca
arindam31 <thearc31@gmail.com>
2018-09-25 18:45:44 +0530

    Fixed: Date field throw warning text. Filter for candi improved

--- Evaluator/filters.py
+++ Evaluator/filters.py
@@ -9,6 +9,9 @@ class InterviewFilter(django_filters.FilterSet):
 
 
 class CandidateFilter(django_filters.FilterSet):
+    start_date = django_filters.DateFilter(name='created_at',lookup_expr=('gt'),)
+    end_date = django_filters.DateFilter(name='created_at',lookup_expr=('lt'),)
+    registry_year = django_filters.DateRangeFilter(field_name='created_at', lookup_expr='year')
 
     class Meta:
         model = Candidate
